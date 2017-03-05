70c334e1f006c7a089f0b5d58c9e74894589d9bd
JefferyTsai <momo123163@gmail.com>
2017-03-05 11:54:19 +0800

    Initial Commit

---
+++ 一秒落淚/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  一秒落淚
+//
+//  Created by Jeffery Tsai on 2017/3/5.
+//  Copyright © 2017年 Jeffery Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
